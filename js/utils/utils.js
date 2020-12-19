154da3530f22f66bd193889154bf6e798610fb1a
tayyabferozi <ferozitayyab@gmail.com>
2020-12-19 05:38:49 +0500

    save cookies for state

--- js/utils/utils.js
+++ js/utils/utils.js
@@ -24,3 +24,23 @@ export const isEmpty = (value) =>
 export const generateErrorMarkup = (message) => {
   return `<h4 class='text-danger col-12 text-center'>${message}</h4>`;
 };
+
+export const createCookie = (name, value, days) => {
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    var expires = "; expires=" + date.toGMTString();
+  } else var expires = "";
+  document.cookie = name + "=" + value + expires + "; path=/";
+};
+
+export const readCookie = (name) => {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+};
